5589260f002161a7f6269e3f2e60de628853ef5e
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-05-26 13:41:44 +0100

    fix: export markdown library from index file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { initAll as initGovUK } from 'govuk-frontend'
 import { initAll as initPrototype } from '@x-govuk/govuk-prototype-components'
+export * from './lib/markdown'
 
 export function initAll() {
   initGovUK()
